849761f4c82b26bb37a3bec1e8231f66f1f65c80
Rodrigo Porto <vgnporto@gmail.com>
2023-04-14 13:02:59 -0300

    feat: script to homologate clinics from sheet

---
+++ src/application/scripts/homologateClinicFromSheet.js
@@ -0,0 +1,34 @@
+// sample data
+const sheet = "sample_data/PlanilhaDeHomologacao.xlsx";
+
+const vtexHomologation = require("../clinicHomologation/");
+const jennerHomologation = require("../clinicHomologation/");
+
+const createClinicFromSheetJSON = require("../../clinic/interfaces/controllers/createClinicFromSheetJSON");
+
+const { createJSONFromSheet } = require("../../infra/xlsx/sheet");
+
+const homologateClinicFromSpreadsheet = async (file, sheetName) => {
+  const json = createJSONFromSheet(file, sheetName);
+
+  for (const item of json) {
+    const clinic = createClinicFromSheetJSON(item);
+    const vtexResults = await vtexHomologation(clinic);
+    const {
+      warehouse: { id: warehouseId },
+      shippingPolicy: { shippingMethod: slasIds },
+    } = vtexResults;
+
+    const jennerResults = await jennerHomologation(clinic, {
+      warehouseId,
+      slasIds: [slasIds],
+    });
+    console.log({
+      ...vtexResults,
+      ...jennerResults,
+    });
+  }
+};
+
+(async () =>
+  console.log(await homologateClinicFromSpreadsheet(sheet, "Sheet1")))();
